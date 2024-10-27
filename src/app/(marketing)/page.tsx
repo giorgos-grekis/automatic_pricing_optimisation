b9ec8189efd9ef173170272da2bd07221edd7de4
giorgos-grekis <georgrekis.github@gmail.com>
2024-10-28 00:19:04 +0200

    complete pricing section

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,9 +1,13 @@
 import { Button } from "@/components/ui/button";
 import { SignUpButton } from "@clerk/nextjs";
-import { ArrowRightIcon } from "lucide-react";
+import { ArrowRightIcon, CheckIcon } from "lucide-react";
 import Link from "next/link";
 import { NeonIcon } from "./_icons/Neon";
 import { ClerkIcon } from "./_icons/Clerk";
+import { subscriptionTiersInOrder } from "@/data/subscriptionTiers";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { formatCompactNumber } from "@/lib/formatCompactNumber";
+import { cn } from "@/lib/utils";
 
 export default function HomePage() {
   return (
@@ -21,6 +25,7 @@ export default function HomePage() {
         </SignUpButton>
       </section>
 
+      {/* Trusted section */}
       <section className="bg-primary text-primary-foreground">
         <div className="container py-16 flex flex-col gap-16 px-8 md:px-16">
           <h2 className="text-3xl text-center text-balance">Trusted by the top modern companies</h2>
@@ -59,6 +64,80 @@ export default function HomePage() {
           </div>
         </div>
       </section>
+
+      {/* Pricing section */}
+      <section id="pricing" className="px-8 py-16 bg-accent/5">
+        <h2 className="text-4xl text-center text-balance font-semibold mb-8">Pricing software which pays for itself 20x over</h2>
+        <div className="grid grid-col-2 lg:grid-cols-4 gap-4 max-w-screen-xl mx-auto">
+          {(subscriptionTiersInOrder ?? []).map((tier) => (
+            <PricingCard key={tier.name} {...tier} />
+          ))}
+        </div>
+      </section>
     </>
+  );
+}
+
+/**
+ * typeof subscriptionTiersInOrder)[number]
+ * means give me the type of one of the elements
+ * inside this array
+ */
+function PricingCard({
+  name,
+  priceInCents,
+  maxNumberOfProducts,
+  maxNumberOfVisits,
+  canAccessAnalytics,
+  canCustomizeBanner,
+  canRemoveBranding,
+  stripePriceId,
+}: (typeof subscriptionTiersInOrder)[number]) {
+  // const isMostPopular = name === "Standard";
+
+  const btnVariant = name === "Standard" ? "accent" : "default";
+
+  return (
+    <Card>
+      <CardHeader>
+        <div className="text-accent font-semibold mb-8">{name}</div>
+        <CardTitle className="text-xl font-bold">${priceInCents / 100} / mo</CardTitle>
+        <CardDescription>{formatCompactNumber(maxNumberOfVisits)} pricing page visits /mo</CardDescription>
+      </CardHeader>
+
+      <CardContent>
+        <SignUpButton>
+          <Button className="text-lg w-full rounded-lg" variant={btnVariant}>
+            Get Started
+          </Button>
+        </SignUpButton>
+      </CardContent>
+
+      <CardFooter className="flex flex-col gap-4 items-start">
+        <Feature className="font-bold">
+          {maxNumberOfProducts} {maxNumberOfProducts === 1 ? "product" : "products"}
+        </Feature>
+
+        <Feature>APO Discounts</Feature>
+        {canAccessAnalytics && <Feature>Advanced analytics</Feature>}
+        {canRemoveBranding && <Feature>Remove Easy APO branding</Feature>}
+        {canCustomizeBanner && <Feature>Banner customization</Feature>}
+      </CardFooter>
+    </Card>
+  );
+}
+
+function Feature({
+  children,
+  className,
+}: Readonly<{
+  children: React.ReactNode;
+  className?: string;
+}>) {
+  return (
+    <div className={cn("flex items-center gap-2", className)}>
+      <CheckIcon className="size-4 stroke-accent bg-accent/25 rounded-full p-0.5" />
+      <span>{children}</span>
+    </div>
   );
 }
