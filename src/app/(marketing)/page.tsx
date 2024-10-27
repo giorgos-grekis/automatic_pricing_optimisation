775312d25feaf1a0e2072c98598bc00f87558cd9
giorgos-grekis <georgrekis.github@gmail.com>
2024-10-27 23:26:30 +0200

    complete the first homepage section

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,3 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { SignUpButton } from "@clerk/nextjs";
+import { ArrowRightIcon } from "lucide-react";
+
 export default function HomePage() {
-  return null;
+  return (
+    <>
+      <section className="min-h-screen bg-[radial-gradient(hsl(0,72%,65%,40%),hsl(24,62%,73%,40%),hsl(var(--background))_60%)] flex items-center justify-center text-center text-balance flex-col gap-8 px-4">
+        {/* generally when we have larger font size it's better to used a tracking-tight because it makes your font a little bit easier to read */}
+        <h1 className="text-6xl lg:text-7xl xl:text-8xl font-bold tracking-tight m-4">Price Smarter, Sell bigger!</h1>
+        <p className="text-lg lg:text-3xl max-w-screen-xl">
+          Optimize your product pricing across countries to maximize sales. Capture 85% of the untapped market with location-based dynamic pricing
+        </p>
+        <SignUpButton>
+          <Button className="text-lg p-6 rounded-lg flex gap-2">
+            Get started for free <ArrowRightIcon className="size-5" />
+          </Button>
+        </SignUpButton>
+      </section>
+    </>
+  );
 }
