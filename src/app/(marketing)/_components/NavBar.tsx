77d511ecb2489a0d0f10b69ac7222b90fbdcdc3f
giorgos-grekis <georgrekis.github@gmail.com>
2024-10-27 23:09:36 +0200

    set up clerk

--- src/app/(marketing)/_components/NavBar.tsx
+++ src/app/(marketing)/_components/NavBar.tsx
@@ -1,4 +1,5 @@
 import { BrandLogo } from "@/components/BrandLogo";
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
 import Link from "next/link";
 
 export function NavBar() {
@@ -17,7 +18,14 @@ export function NavBar() {
         <Link className="text-lg" href={"#"}>
           About
         </Link>
-        <span className="text-lg"></span>
+        <span className="text-lg">
+          <SignedIn>
+            <Link href={"/dashboard"}>Dashboard</Link>
+          </SignedIn>
+          <SignedOut>
+            <SignInButton>Login</SignInButton>
+          </SignedOut>
+        </span>
       </nav>
     </header>
   );
